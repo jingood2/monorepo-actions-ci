1c375f9a3978c1f54fa10497073ae62e4637478b
jingood2 <jingood2@gmail.com>
2022-05-22 05:45:24 +0900

    update feat-test

---
+++ generate-lambda-cicd-workflow.sh
@@ -0,0 +1,14 @@
+# read the workflow template
+WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
+SAM_WORKFLOW_TEMPLATE=$(cat .github/lambda-cicd-template.yaml)
+
+# iterate each route in routes directory
+for ROUTE in $(ls projects); do
+    echo "generating workflow for projects/${ROUTE}"
+
+    # replace template route placeholder with route name
+    WORKFLOW=$(echo "${SAM_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+
+    # save workflow to .github/workflows/{ROUTE}
+    echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
+done
