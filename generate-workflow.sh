2294edb508663848d682525335a2ac740b036265
jingood2 <jingood2@gmail.com>
2022-05-24 16:48:01 +0900

    v25

--- generate-workflow.sh
+++ generate-workflow.sh
@@ -3,30 +3,30 @@ WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
 SAM_WORKFLOW_TEMPLATE=$(cat .github/sam-cicd-template.yaml)
 MULTI_ACCOUNT_WORKFLOW_TEMPLATE=$(cat .github/multi-account-deploy-template.yml)
 
-TARGET_DEPLOY_ACCOUNTS=['037729278610']
+TARGET_DEPLOY_ACCOUNTS=('037729278610' '123429278610')
 
-#for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS}: do
-#    for ROUTE in $(ls projects); do
-#        echo "generating workflow for projects/${ROUTE}"
-#
-#        # replace template route placeholder with route name
-#        #WORKFLOW=$(echo "${SAM_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
-#        WORKFLOW=$(echo "${MULTI_ACCOUNT_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
-#        WORKFLOW=$(echo "${MULTI_ACCOUNT_WORKFLOW_TEMPLATE}" | sed "s/{{ACCOUNT}}/${ACCOUNT}/g")
-#
-#        # save workflow to .github/workflows/{ROUTE}
-#        mkdir-
-#        echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
-#    done
-#done
+for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS[@]}; do
+    for ROUTE in $(ls projects); do
+        echo "generating workflow for projects/${ROUTE}"
+
+        # replace template route placeholder with route name
+        #WORKFLOW=$(echo "${SAM_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+        WORKFLOW=$(echo "${MULTI_ACCOUNT_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+        WORKFLOW=$(echo "${WORKFLOW}" | sed "s/{{ACCOUNT}}/${ACCOUNT}/g")
+
+        # save workflow to .github/workflows/{ROUTE}
+        mkdir -p .github/workflows/${ACCOUNT}
+        echo "${WORKFLOW}" > .github/workflows/${ACCOUNT}/${ROUTE}.yaml
+    done
+done
 
 #iterate each route in routes directory
-for ROUTE in $(ls projects); do
-    echo "generating workflow for projects/${ROUTE}"
-
-    # replace template route placeholder with route name
-    WORKFLOW=$(echo "${MULTI_ACCOUNT_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
-
-    # save workflow to .github/workflows/{ROUTE}
-    echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
-done
+#for ROUTE in $(ls projects); do
+#    echo "generating workflow for projects/${ROUTE}"
+#
+#    # replace template route placeholder with route name
+#    WORKFLOW=$(echo "${MULTI_ACCOUNT_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+#
+#    # save workflow to .github/workflows/{ROUTE}
+#    echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
+#done
