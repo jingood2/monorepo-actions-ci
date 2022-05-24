69e795086084040ea38c495a59821ce12634e593
jingood2 <jingood2@gmail.com>
2022-05-24 18:52:26 +0900

    v34

--- projects/hello-world/hello-world/app.js
+++ projects/hello-world/hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world v33' 
+                message: 'hello world v34' 
                 // location: ret.data.trim()
             })
         }
