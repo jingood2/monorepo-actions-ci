7b69fcd5deec8e2f97d3800a784074209fb4dcd0
jingood2 <jingood2@gmail.com>
2022-05-25 07:23:37 +0900

    v2

--- generate-workflow-v3.sh
+++ generate-workflow-v3.sh
@@ -1,22 +1,28 @@
 # read the workflow template
-WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
 SAM_WORKFLOW_TEMPLATE=$(cat .github/sam-cicd-template.yaml)
-MULTI_ACCOUNT_WORKFLOW_TEMPLATE=$(cat .github/multi-account-reusable-cd-template.yml)
 CD_WORKFLOW_TEMPLATE=$(cat .github/multi-account-cd-template.yml)
+
+cp .github/multi-account-cd-template.yml /tmp/multi-account-cd.yml
+CD_WORKFLOW_TEMP=$(cat /tmp/multi-account-cd.yml)
+
+cp .github/multi-account-reusable-cd-template.yml /tmp/multi-account-reusable-cd.yml
 
 BUILD_ACCOUNT='037729278610'
 TARGET_DEPLOY_ACCOUNTS=('037729278610' '123429278610')
 
+# ACCOUNT 수만큼 reusable template에 deploy-prod 추가
+for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS[@]}; do
+    CD_WORKFLOW=$(echo "${CD_WORKFLOW_TEMP}" | sed "s/{{ACCOUNT}}/${ACCOUNT}/g")
+    echo "${CD_WORKFLOW}" >> /tmp/multi-account-reusable-cd.yml
+done
 
+MULTI_ACCOUNT_WORKFLOW_TEMPLATE=$(cat /tmp/multi-account-reusable-cd.yml)
+#MULTI_ACCOUNT_WORKFLOW_TEMPLATE=$(cat .github/multi-account-reusable-cd-template.yml)
 
 for ROUTE in $(ls projects); do
     echo "generating workflow for projects/${ROUTE}"
 
-    # ACCOUNT 수만큼 reusable template에 deploy-prod 추가
-    for ACCOUNT in ${TARGET_DEPLOY_ACCOUNTS[@]}; do
-        CD_WORKFLOW=$(echo "${CD_WORKFLOW_TEMPLATE}" | sed "s/{{ACCOUNT}}/${ACCOUNT}/g")
-        echo "${CD_WORKFLOW}" >> .github/multi-account-reusable-cd-template.yml
-    done
+   
 
     # replace template route placeholder with route name
     #WORKFLOW=$(echo "${SAM_WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
